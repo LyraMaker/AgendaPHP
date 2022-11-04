0eb70f0dc765aae2f72f46246a2c3cd3336a769c
LyraMaker <gabriel.lyrat@gmail.com>
2022-11-04 07:17:55 -0300

    Index - Criado

---
+++ view/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8" />
+	<title>Agenda PHP | 🗒️</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+	<link rel="icon" type="image/x-icon" href="favicon.png">
+</head>
+<body>
+<header class="bg-primary text-light text-uppercase d-flex justify-content-center p-4"><h1>Agenda em PHP</h1></header>
+	<main class="container py-4">
+	
